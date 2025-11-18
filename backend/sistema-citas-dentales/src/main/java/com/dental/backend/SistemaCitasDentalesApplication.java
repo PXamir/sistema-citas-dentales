2f54d68fd52614619a337f430d6f0b1bb46a013c
PXamir <166246624+PXamir@users.noreply.github.com>
2025-11-18 18:35:03 -0500

    feat: estructura inicial del proyecto backend Spring Boot y configuración del application.properties
    Creación del proyecto en Spring Tool Suite 4.
    
    Configuración:
    Type: Maven
    Language: Java
    Java version: 21
    Packaging: Jar
    
    Group:
    com.dental
    
    Package:
    com.dental.backend

---
+++ backend/sistema-citas-dentales/src/main/java/com/dental/backend/SistemaCitasDentalesApplication.java
@@ -0,0 +1,13 @@
+package com.dental.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCitasDentalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCitasDentalesApplication.class, args);
+	}
+
+}
---
+++ backend/sistema-citas-dentales/src/test/java/com/dental/backend/SistemaCitasDentalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dental.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCitasDentalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
